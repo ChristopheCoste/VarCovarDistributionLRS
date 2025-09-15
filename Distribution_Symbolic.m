7629feefcff2ada60b64aab0fe55e506acc3b17c
ChristopheCoste <43001323+ChristopheCoste@users.noreply.github.com>
2025-09-15 10:56:56 +0200

    Update Distribution_Symbolic.m

--- Distribution_Symbolic.m
+++ Distribution_Symbolic.m
@@ -1,41 +1,39 @@
-clear all; clc;close all;
-
 %% inputs
 s=2; % number of states
 
-S=[.3 0; .2 .7] % expectation of survival
+S=[.3 0; .2 .7] % expectation of survival, eq (a)
 
 x = sym('x_%d',[1 s]); % dummy variable of length s
 y = sym('y'); %dummy univariate variable
 
-pgfF=[x(1)/2 + 1/2, x(1)/2 + x(2)/2] % pgf of reproduction 
+pgfF=[x(1)/2 + 1/2, x(1)/2 + x(2)/2] % pgf of reproduction, eq (d)
 
 %% intermediary outputs
 
-pgfS  = ones(1,s) +  (x- ones(1,s))*S; %pgf of survival (eq 4)
+pgfS  = ones(1,s) +  (x- ones(1,s))*S; %pgf of survival; eqs 4 and (b)
 vpa(pgfS,2)
 
 
 %F=[0.5 0.5; 0 0.5] %expectation of reproduction
 
 
-F=expect_pgf(pgfF,x,s,s) % expectation of reproduction
-VF=Var_pgf(pgfF,x,s,s)% variance covaraince of reproduction
+F=expect_pgf(pgfF,x,s,s) % expectation of reproduction, eq (e)
+VF=Var_pgf(pgfF,x,s,s)% variance covaraince of reproduction, eq (e)
 % these two elements are not necessary to compute the pgf of LRS
 
 %% pgf n1
-pgfn0=x(1)*x(2)
+pgfn0=x(1)*x(2) % eq (f)
 
-pgfn1=subs(pgfn0,x,pgfF.*pgfS)
+pgfn1=subs(pgfn0,x,pgfF.*pgfS) % eq (h)
 
-expand(pgfn1)
+expand(pgfn1) % eq (h)
+
 
 %% pgf of R
 
 D=(eye(s)-S*diag(pgfF))
-D^(-1)
 
-pgfR=(pgfF + ones(1,s)*D - ones(1,s))*D^(-1) %eq 14
+pgfR=(pgfF + ones(1,s)*D - ones(1,s))*D^(-1) %eqs 17 and (k)
 
 %% pgf of total LRS (rT)
 pgff=subs(pgfF,x,y*ones(1,s)); %pgf of univariate (i.e. unstructured) reproduction
@@ -69,41 +67,46 @@ end
 
 %% figure
 
-nmax3d=8
+nmax3d=8;
 
 figure
 t = tiledlayout(2,2,'TileSpacing','Compact','Padding','Compact');
 nexttile;
 bar3(Dist(1:nmax3d+1,1:nmax3d+1,1),'b')
-xlabel('type \emph{small} offspring','Interpreter','latex')
-ylabel('type \emph{large} offspring','Interpreter','latex')
+ylabel('\emph{small} offspring','Interpreter','latex')
+xlabel('\emph{large} offspring','Interpreter','latex')
 zlabel('probability','Interpreter','latex')
 title('(a): joint distribution of LRS of \emph{small} type: ${\mathbf{R_1}}$','Interpreter','latex')
 view(-135,20)
 set(gca,'XTickLabel',[0:nmax3d])
 set(gca,'YTickLabel',[0:nmax3d])
+view(45,20)
+zlim([0 0.4])
 
 nexttile;
 bar3(Dist(1:nmax3d+1,1:nmax3d+1,2),'r')
-xlabel('type \emph{small} offspring','Interpreter','latex')
-ylabel('type \emph{large} offspring','Interpreter','latex')
+ylabel('\emph{small} offspring','Interpreter','latex')
+xlabel('\emph{large} offspring','Interpreter','latex')
 zlabel('probability','Interpreter','latex')
 title('(b): joint distribution of LRS of \emph{large} type: ${\mathbf{R_2}}$','Interpreter','latex')
 view(-135,20)
 set(gca,'XTickLabel',[0:nmax3d])
 set(gca,'YTickLabel',[0:nmax3d])
+view(45,20)
+zlim([0 0.4])
 
 nexttile;
 bar(distres1(1,:),distres1(2,:),'b')
 xlabel('lifetime reproductive success','Interpreter','latex')
 ylabel('probability','Interpreter','latex')
 title('(c): distribution of total LRS of \emph{small} type: $r(1)$','Interpreter','latex')
-
+ylim([0 0.4])
 nexttile;
 bar(distres2(1,:),distres2(2,:),'r')
 xlabel('lifetime reproductive success','Interpreter','latex')
 ylabel('probability','Interpreter','latex')
 title('(d): distribution of total LRS of \emph{large} type: $r(2)$','Interpreter','latex')
+ylim([0 0.4])
 
 %% functions
 
